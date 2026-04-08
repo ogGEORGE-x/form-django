7f0a57704a9a3ca4ff7fe9b73c3bb1a4bc847986
ogGEORGE-x <gfurious01@gmail.com>
2026-04-08 07:11:20 -0400

    Index modified

--- jinjapro3/settings.py
+++ jinjapro3/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'jinjapro3.urls'
@@ -73,12 +74,13 @@ WSGI_APPLICATION = 'jinjapro3.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600)
     }
-}
+
 
 
 # Password validation
